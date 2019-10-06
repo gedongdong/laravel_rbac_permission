fc20eac0e773b79d9d3c094d93716335cd15f5fb
gedongdong <gedongdong2010@163.com>
2019-10-06 10:30:23 +0800

    login.css

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="stylesheet" href="/js/layui/css/layui.css" media="all">
-    <link rel="stylesheet" href="/js/layui/css/login.css" media="all">
+    <link rel="stylesheet" href="/css/login.css" media="all">
 </head>
 <body>
 
